20586c5c82a2d4f0b7bc10b5f2d7a747aeb96cf1
sandiaPop <elohim.guevara@gmail.com>
2016-08-15 22:52:29 -0500

    constructed letter k shape

--- week-7/assignment-7/sketch.js
+++ week-7/assignment-7/sketch.js
@@ -16,10 +16,12 @@
 var kim;
 
 function preload() {
+  // load an image
   kim = loadImage("kim.jpg");
 }
 
 function setup() {
+  // size of the canvas
   createCanvas(300, 400);
 }
 
@@ -27,4 +29,23 @@ function draw() {
   image(kim);
   fill(240, 70, 218, 130);
   rect(0, 0, 300, 400);
+
+  // letter "K"
+  fill(244, 070, 017);
+  noStroke();
+  beginShape();
+  vertex(107.33, 94.33);
+  vertex(107.33, 274.59);
+  vertex(129.33, 274.59);
+  vertex(129.33, 229);
+  vertex(141, 215.67);
+  vertex(183, 274.59);
+  vertex(210.67, 274.59);
+  vertex(157.33, 202.33);
+  vertex(203.67, 151.5);
+  vertex(176.67, 151.5);
+  vertex(129.67, 208.67);
+  vertex(129.67, 94.33);
+  vertex(107.33, 94.33);
+  endShape();
 }
