8f0772db1dda61c6aedacc6919a236dfdfdeeb4c
sandiaPop <elohim.guevara@gmail.com>
2016-10-06 15:07:10 -0500

    Added index.html and sketch.js

---
+++ week-9/assignment-9/sketch.js
@@ -0,0 +1,11 @@
+/**
+*
+* Author: Elohim Guevara Varela
+*
+* Assignment 8: Data Visualization
+*
+* MOOC: Introduction to Programming For The Visual Arts with p5.js
+*
+* Platform: https://www.kadenze.com/
+*
+*/
