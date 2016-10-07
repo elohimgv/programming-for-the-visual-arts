3db7b7e51bd4da6cc8042739b323f4774a7d7f84
sandiaPop <elohim.guevara@gmail.com>
2016-10-07 11:31:46 -0500

    Established a set of functions to draw a table to display data

--- week-9/assignment-9/sketch.js
+++ week-9/assignment-9/sketch.js
@@ -11,21 +11,43 @@
 *
 * The information was collected frow my personal twitter account
 * with app TAGS https://tags.hawksey.info/
+* 66354 tweets from #climatechange
 *
 * Platform: https://www.kadenze.com/
 *
 */
 
+// var to file csv
 var climateChange;
 
+// to load climatechange.csv file
 function preload() {
   climateChange = loadTable("climatechange.csv", "csv", "header");
 }
 
 function setup() {
+  // size of canvas
   createCanvas(windowWidth, 600);
+  // font for write on data Visualization
+  textFont("Noto Sans");
 }
 
 function draw() {
+  // background color
   background(0);
+  // time line and number of tweets
+  // color lines
+  stroke(255);
+  // color font
+  fill(255);
+  // size font
+  textSize(15);
+  // axis x = days
+  line(70, 540, 1270, 540);
+  // write days
+  text("days", 1230, 565);
+  // axis y = number of tweets
+  line(70, 40, 70, 540);
+  // write number of tweets
+  text("tweets", 15, 55);
 }
