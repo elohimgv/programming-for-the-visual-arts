63bb09eb52518d7d861de51d1a90b025cfbb9b0c
sandiaPop <elohim.guevara@gmail.com>
2016-10-06 21:25:55 -0500

    Added climatechange.csv and function preload(), function setup(), function draw()

--- week-9/assignment-9/sketch.js
+++ week-9/assignment-9/sketch.js
@@ -6,6 +6,26 @@
 *
 * MOOC: Introduction to Programming For The Visual Arts with p5.js
 *
+* Note: the code dosen´t run on browser like Crhome, Chrome Canary,
+* Microsoft Edge, just run on Mozilla Firefox.
+*
+* The information was collected frow my personal twitter account
+* with app TAGS https://tags.hawksey.info/
+*
 * Platform: https://www.kadenze.com/
 *
 */
+
+var climateChange;
+
+function preload() {
+  climateChange = loadTable("climatechange.csv", "csv", "header");
+}
+
+function setup() {
+  createCanvas(windowWidth, 600);
+}
+
+function draw() {
+  background(0);
+}
