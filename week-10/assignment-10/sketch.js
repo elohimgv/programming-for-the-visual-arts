13b17ebc52112f598478d339390ad2427e85336a
sandiaPop <elohim.guevara@gmail.com>
2016-10-15 18:51:15 -0500

    Added week-10 folder

---
+++ week-10/assignment-10/sketch.js
@@ -0,0 +1,11 @@
+/**
+*
+* Author: Elohim Guevara Varela
+*
+* Assignment 10: Minimal Game
+*
+* MOOC: Introduction to Programming For The Visual Arts with p5.js
+*
+* Platform: https://www.kadenze.com/
+*
+*/
