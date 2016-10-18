e365c7b0c9a3ce26047ae7af7295603eddc2f9cb
sandiaPop <elohim.guevara@gmail.com>
2016-10-17 20:33:32 -0500

    Added back_image.jpg
    Created tha basis that include global variables, declared objects, function preload(), function setup(), function draw(), class Game() and function keyPressed()

--- week-10/assignment-10/sketch.js
+++ week-10/assignment-10/sketch.js
@@ -9,3 +9,56 @@
 * Platform: https://www.kadenze.com/
 *
 */
+
+// vars to load sounds
+var soundMovement;
+var soundHit;
+// var to load an image background
+var backgroundImage;
+// delcare objects
+var layout = [];
+var score;
+var lives;
+
+
+function preload() {
+  backgroundImage = loadImage("back_image.jpg");
+}
+
+function setup() {
+  // create objects
+  layout = new Game();
+  score = new Game();
+  lives = new Game();
+  // size canvas
+  createCanvas(1000, 500);
+}
+
+function draw() {
+  var RGBcolor = color(255, 0, 0);
+  var score = "0";
+  var lives = "0";
+  // background image
+  image(backgroundImage);
+  // write score
+  text("score", 10, 20);
+  text(score, 50, 20);
+  // write lives
+  text("lives", 10, 40)
+  text(lives, 50, 40);
+  // circle color
+  fill(RGBcolor);
+  // to display circle
+  ellipse(40, 500 / 2, 50, 50);
+  // score and live board
+  // rect(7, 5, 50, 50);
+}
+
+// layout Class
+function Game() {
+
+}
+
+function keyPressed() {
+
+}
