2f52114c3c9ea8a271cc184d0b9dc77e3badd487
sandiaPop <elohim.guevara@gmail.com>
2016-10-17 21:25:31 -0500

    Added instance var for method display()

--- week-10/assignment-10/sketch.js
+++ week-10/assignment-10/sketch.js
@@ -20,22 +20,23 @@ var layout = [];
 var score;
 var lives;
 
-
 function preload() {
   backgroundImage = loadImage("back_image.jpg");
 }
 
 function setup() {
-  // create objects
-  layout = new Game();
-  score = new Game();
-  lives = new Game();
   // size canvas
   createCanvas(1000, 500);
+  // create objects
+  for (var i = 0; i < 5; i++) {
+    layout.push(new Game());
+  }
+  score = new Game();
+  lives = new Game();
 }
 
 function draw() {
-  var RGBcolor = color(255, 0, 0);
+  var RGBcolor = color(255, 255, 255);
   var score = "0";
   var lives = "0";
   // background image
@@ -52,11 +53,32 @@ function draw() {
   ellipse(40, 500 / 2, 50, 50);
   // score and live board
   // rect(7, 5, 50, 50);
+  for (var i = 0; i < layout.length; i++) {
+    layout[i].display();
+  }
 }
 
-// layout Class
+// Game class
 function Game() {
+  // instance variables
+  // up
+  this.x_up = 100;
+  this.y_up = 0;
+  this.sizeWidthUp = 50;
+  this.sizeHeightUp = 200;
+  // down
+  this.x_down = 100;
+  this.y_down = 300;
+  this.sizeWidthDown = 50;
+  this.sizeHeightDown = 200;
 
+  // instance methods
+  this.display = function() {
+    for (var i = 1000; i > 0; i -= 200) {
+      rect(this.x_up + i, this.y_up, this.sizeWidthUp, this.sizeHeightUp);
+      rect(this.x_down + i, this.y_down, this.sizeWidthDown, this.sizeHeightDown);
+    }
+  }
 }
 
 function keyPressed() {
