d96d1e4e62831c099c088e51b8cbd1399253106c
elohim <elohim.guevara@gmail.com>
2020-11-26 10:38:31 -0600

    update assignment-6

--- lesson-6/assignment-6/sketch.js
+++ lesson-6/assignment-6/sketch.js
@@ -1,32 +1,30 @@
 /**
 *
-* Author: Elohim Guevara Varela
+* Author: Elohim Guevara 
 *
 * Assignment 6: Audio Visualizer
-*
-* MOOC: Introduction to Programming For The Visual Arts with p5.js
-*
-* Platform: https://www.kadenze.com/
 *
 */
 
 var sound;
 
 var angle = 0;
-// change the position of the wave in "axis y"
-// if increment the offset value, "axis y" increment and vice versa
+// Change the position of the wave in "axis y"
+// if increment the offset value, "axis y" 
+// increment and vice versa
 var offset = 300;
-// amplitud of the wave
+// Amplitud of the wave
 var scalar = 35;
-// speed of the motion
+// Speed of the motion
 var speed = 0.05;
-// size X baLL
+// Size X baLL
 var sizeX_ball = 10;
-// size Y ball
+// Size Y ball
 var sizeY_ball = 10;
-// boolean var to activate increase and decrease the ball
+// Boolean var to activate increase 
+// and decrease the ball
 var state = true;
-// var move to ball x axis
+// Var move to ball x axis
 var moveX;
 
 function preload() {
@@ -50,15 +48,16 @@ function draw() {
   var G = map(mouseX, 0, width, 0, 255);
   // RGB - BLUE color
   var B = map(mouseY, 0, height, 0, 255);
-  // ball moves on sine curve
+  // Ball moves on sine curve
   var y = offset + (sin(angle) * scalar);
-  // condition to increase the ball if is true
+  // Condition to increase the ball if is true
   if (state == false) {
     fill(234, G, B);
     ellipse(mouseX + 20, y, sizeX_ball, sizeY_ball);
   }
-  // condition to increase the ball if is false
-  // and avoid to disappear the ball when RIGHT_ARROW key is pressed
+  // Condition to increase the ball if is true
+  // and avoid to disappear the ball when 
+  // RIGHT_ARROW key is pressed
   if (state == true) {
     fill(234, G, B);
     ellipse(mouseX + 20, y, sizeX_ball, sizeY_ball);
@@ -72,11 +71,11 @@ function draw() {
 function keyPressed() {
   loop();
   state = !state;
-  // decision to increase the ball
+  // Decision to increase the ball
   if (keyCode == RIGHT_ARROW) {
     sizeX_ball += 10;
     sizeY_ball += 10;
-  // decision to decrease the ball
+  // Decision to decrease the ball
   } else if (keyCode == LEFT_ARROW) {
     sizeX_ball -= 10;
     sizeY_ball -= 10;
